9e1e3b9b5d1805bb8816b7ac7cf3c0143f1c3841
Roger Peters <47227555+rapeters@users.noreply.github.com>
2025-12-05 15:59:56 -0500

    Add answer to 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = Array.from(str.replace(/[\W\s]/gi, '').toLowerCase());
+    let revArr = arr.slice().reverse();
+    return arr.join() === revArr.join();
 };
 
 // Do not edit below this line
