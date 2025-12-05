a1ceb2918d70760717898bbc97d17f58daddac59
Roger Peters <47227555+rapeters@users.noreply.github.com>
2025-12-05 15:07:51 -0500

    Use reduce array method for answers to 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,41 +1,29 @@
 const add = function(a, b) {
-	return a + b;
+  return a + b;
 };
 
 const subtract = function(a, b) {
-	return a - b;
+  return a - b;
 };
 
 const sum = function(arr) {
-	let sum = 0;
-  arr.forEach(element => {
-    sum += element;
-  });
-  return sum;
+  return arr.reduce((sum, current) => sum + current, 0);
 };
 
 const multiply = function(arr) {
-  let product = 1;
-  arr.forEach(element => {
-    product *= element;
-  })
-  return product;
+  return arr.reduce((product, current) => product * current, 1);
 };
 
 const power = function(base, exponent) {
-	return base ** exponent;
+  return base ** exponent;
 };
 
 const factorial = function(n) {
-	if (n === 0) {
-    return 1;
-  } else {
-    let product = n;
-    for (let i = n; i > 1; i--) {
-      product *= (i - 1);
-    }
-    return product;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
   }
+  return product;
 };
 
 // Do not edit below this line
