b26076a167f95f3925055db252e828c93f04b2bb
Roger Peters <47227555+rapeters@users.noreply.github.com>
2025-12-05 14:59:21 -0500

    Add solution to exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  arr.forEach(element => {
+    product *= element;
+  })
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) {
+    return 1;
+  } else {
+    let product = n;
+    for (let i = n; i > 1; i--) {
+      product *= (i - 1);
+    }
+    return product;
+  }
 };
 
 // Do not edit below this line
