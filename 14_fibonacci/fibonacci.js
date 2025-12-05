9baced6443acca29e6950ab260665b0ab0829fca
Roger Peters <47227555+rapeters@users.noreply.github.com>
2025-12-05 16:36:10 -0500

    Add answer to 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let n = parseInt(num);
+    
+    if (n === 0) return 0;
+    if (n < 0) return "OOPS"
 
+    let arr = [1, 1];
+    while (arr.length < n) {
+        arr.push(arr[arr.length-1] + arr[arr.length-2]);
+    }
+    return arr[arr.length-1];
 };
 
 // Do not edit below this line
